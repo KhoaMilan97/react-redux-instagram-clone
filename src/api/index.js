1d46e555de74945f3abaf8e98fa1321f6aa714f5
KhoaMilan97 <KhoaMilan97>
2021-06-11 09:59:00 +0700

    fix url default

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
   headers: { crossDomain: true, "Content-Type": "application/json" },
 });
